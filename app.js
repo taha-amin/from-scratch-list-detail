4a702d656d0a7134126b1b5a5c4de3ebc32188c8
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-22 17:04:13 -0700

    in app.js imported from fetch file and render file. created event listener so that on load fetches all books then renders and appends all boks to the container.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
+import { getBooks } from './fetch-utils.js';
+import { renderBookCard } from './render-utils.js';
 
-// let state
+const bookListContainer = document.getElementById('book-list-container');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//on load
+//fetch all books
+//render and append all book cards to the container
+
+window.addEventListener('load', async () => {
+    //send request for all dogs
+      // wait for response
+    const books = await getBooks();
+
+    //once response shows up, render and append
+    for (let book of books) {
+        const bookEl = renderBookCard(book);
+
+        bookListContainer.append(bookEl);
+    }
+});
